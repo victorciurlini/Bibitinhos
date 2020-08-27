ec0f07271377d609413ce231512d42f0cc02c966
Victor Ciurlini <victorciurlini@gmail.com>
2020-08-27 20:55:32 -0300

    :sparkles: adicionando movimentos aos bibitys

---
+++ Bibitinhos/Assets/Scripts/Bibity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Bibity : MonoBehaviour {
+
+ 	public float speed;//setting the speed the character will rotate
+	private Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+void FixedUpdate(){
+	float moveHorizontal = Input.GetAxis("Horizontal");
+	float moveVertical = Input.GetAxis("Vertical");
+	Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+	rb.AddForce(movement*speed);
+	}
+}
